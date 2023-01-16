1bcd2e57eed74c0001f445052575a930bf77f4a4
Linmuz <120557379+Linmuz@users.noreply.github.com>
2023-01-16 20:54:34 +0800

    Update board_c3_hid1.h

--- main/boards/board_c3_hid1.h
+++ main/boards/board_c3_hid1.h
@@ -3,7 +3,7 @@
 #include "board_common.h"
 
 #define BOARD_HAS_LED
-#define BOARD_PIN_LED   (7)   
+#define BOARD_PIN_LED   (3)   
 
 /*
 #define BOARD_HAS_FINGERPRINT_MODULE
@@ -17,7 +17,7 @@
 #define BOARD_BATTARY_VOLTAGE_SCALE (2)
 */
 
-#define BOARD_USB1_PIN_DP   (3)
-#define BOARD_USB1_PIN_DM   (4)
-#define BOARD_USB2_PIN_DP   (5)
-#define BOARD_USB2_PIN_DM   (6)
+#define BOARD_USB1_PIN_DP   (4)
+#define BOARD_USB1_PIN_DM   (5)
+#define BOARD_USB2_PIN_DP   (6)
+#define BOARD_USB2_PIN_DM   (7)
